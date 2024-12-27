1264d57b0b3c381527d137959761f069ea430fb9
Talha Gergin <talhagergin6@gmail.com>
2024-12-28 02:22:05 +0300

    Initial Commit

---
+++ OnboardingScreen/OnboardingScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingScreenApp.swift
+//  OnboardingScreen
+//
+//  Created by Talha Gergin on 28.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
